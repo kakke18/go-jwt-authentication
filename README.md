e711876181afdc5e59b6ba920c269956d2138001
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-06-10 12:47:02 +0900

    add: handson

--- README.md
+++ README.md
@@ -5,5 +5,18 @@ Implement jwt authentication in Go
 ## Reference
 [Go言語で理解するJWT認証実装ハンズオン](https://qiita.com/po3rin/items/740445d21487dfcb5d9f)
 
+## Usage
+```
+cp .env.example .env
+gp run main.go
+
+curl localhost:8080/public
+curl localhost:8080/auth
+curl localhost:8080/private "Authorization: Bearer {JWT token}"
+```
+
+## Link
+[jwt](https://jwt.io/)
+
 ## Author
 [kakke18](https://github.com/kakke18)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+
+    "github.com/kakke18/go-jwt-authentication/auth"
+)
+
+type post struct {
+    Title string `json:"title"`
+    Tag   string `json:"tag"`
+    URL   string `json:"url"`
+}
+
+func main() {
+    r := mux.NewRouter()
+	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
+
+    if err := http.ListenAndServe(":8080", r); err != nil {
+        log.Fatal("ListenAndServe:", nil)
+    }
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    post := &post{
+        Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
+        Tag:   "Vue.js",
+        URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+    }
+    json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    post := &post{
+        Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
+        Tag:   "Go",
+        URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
+    }
+    json.NewEncoder(w).Encode(post)
+})
